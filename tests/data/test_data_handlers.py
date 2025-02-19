4746659d91551263ed3f47b7e39b517f2100caae
Julien Raynal <julien@Juliens-MacBook-Pro.local>
2025-02-19 11:36:45 +0100

    REPLACE: tests/data/test_data_handlers.py -> Changed YamlTransformColumnsTransformation to TransformColumnsTransformation

--- tests/data/test_data_handlers.py
+++ tests/data/test_data_handlers.py
@@ -18,7 +18,7 @@ from stimulus.utils.yaml_data import (
     YamlSplitTransformDict,
     YamlTransform,
     YamlTransformColumns,
-    YamlTransformColumnsTransformation,
+    TransformColumnsTransformation,
     generate_split_configs,
     generate_split_transform_configs,
 )
@@ -70,8 +70,7 @@ def generate_sub_configs(base_config: YamlConfigDict) -> list[YamlConfigDict]:
     Returns:
         list[YamlConfigDict]: List of generated configurations
     """
-    split_configs: list[YamlSplitConfigDict] = generate_split_configs(
-        base_config)
+    split_configs: list[YamlSplitConfigDict] = generate_split_configs(base_config)
     split_transform_list: list[YamlSplitTransformDict] = []
     for split in split_configs:
         split_transform_list.extend(generate_split_transform_configs(split))
@@ -104,8 +103,7 @@ def encoder_loader(generate_sub_configs: list[YamlConfigDict]) -> loaders.Encode
         experiments.EncoderLoader: Initialized encoder loader
     """
     loader = loaders.EncoderLoader()
-    loader.initialize_column_encoders_from_config(
-        generate_sub_configs[0].columns)
+    loader.initialize_column_encoders_from_config(generate_sub_configs[0].columns)
     return loader
 
 
@@ -238,7 +236,7 @@ def test_transform_manager_transform_column() -> None:
             YamlTransformColumns(
                 column_name="test_col",
                 transformations=[
-                    YamlTransformColumnsTransformation(
+                    TransformColumnsTransformation(
                         name="GaussianNoise",
                         params={"std": 0.1},
                     ),
@@ -246,12 +244,10 @@ def test_transform_manager_transform_column() -> None:
             ),
         ],
     )
-    transform_loader.initialize_column_data_transformers_from_config(
-        dummy_config)
+    transform_loader.initialize_column_data_transformers_from_config(dummy_config)
     manager = TransformManager(transform_loader)
     data = [1, 2, 3]
-    transformed, added_row = manager.transform_column(
-        "test_col", "GaussianNoise", data)
+    transformed, added_row = manager.transform_column("test_col", "GaussianNoise", data)
     assert len(transformed) == len(data)
     assert added_row is False
 
@@ -334,28 +330,22 @@ def test_dataset_processor_apply_transformation_group(
         transform_manager=TransformManager(transform_loader)
     )
 
-    assert processor.data["age"].to_list(
-    ) != processor_control.data["age"].to_list()
-    assert processor.data["fare"].to_list(
-    ) != processor_control.data["fare"].to_list()
+    assert processor.data["age"].to_list() != processor_control.data["age"].to_list()
+    assert processor.data["fare"].to_list() != processor_control.data["fare"].to_list()
     assert (
-        processor.data["parch"].to_list(
-        ) == processor_control.data["parch"].to_list()
+        processor.data["parch"].to_list() == processor_control.data["parch"].to_list()
     )
     assert (
-        processor.data["sibsp"].to_list(
-        ) == processor_control.data["sibsp"].to_list()
+        processor.data["sibsp"].to_list() == processor_control.data["sibsp"].to_list()
     )
     assert (
-        processor.data["pclass"].to_list(
-        ) == processor_control.data["pclass"].to_list()
+        processor.data["pclass"].to_list() == processor_control.data["pclass"].to_list()
     )
     assert (
         processor.data["embarked"].to_list()
         == processor_control.data["embarked"].to_list()
     )
-    assert processor.data["sex"].to_list(
-    ) == processor_control.data["sex"].to_list()
+    assert processor.data["sex"].to_list() == processor_control.data["sex"].to_list()
 
 
 # Test DatasetLoader
